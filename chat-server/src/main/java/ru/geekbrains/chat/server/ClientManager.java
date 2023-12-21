8af55be5dad1d798254361899f0593cb43fab1aa
Artem Maklashev <artem@maklashev.ru>
2023-12-21 13:27:34 +0400

    lesson5 done

--- chat-server/src/main/java/ru/geekbrains/chat/server/ClientManager.java
+++ chat-server/src/main/java/ru/geekbrains/chat/server/ClientManager.java
@@ -2,7 +2,10 @@ package ru.geekbrains.chat.server;
 
 import java.io.*;
 import java.net.Socket;
+import java.sql.Struct;
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class ClientManager implements Runnable {
 
@@ -11,7 +14,7 @@ public class ClientManager implements Runnable {
     private BufferedWriter bufferedWriter;
     private String name;
 
-    public final static ArrayList<ClientManager> clients = new ArrayList<>();
+    public final static Map<String, ClientManager> clients = new TreeMap<>();
 
     public ClientManager(Socket socket) {
         this.socket = socket;
@@ -19,11 +22,10 @@ public class ClientManager implements Runnable {
             bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
             bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             name = bufferedReader.readLine();
-            clients.add(this);
+            clients.put(name, this);
             System.out.println(name + " подключился к чату.");
             broadcastMessage("Server: " + name + " подключился к чату.");
-        }
-        catch (IOException e){
+        } catch (IOException e) {
             closeEverything(socket, bufferedReader, bufferedWriter);
         }
 
@@ -43,26 +45,53 @@ public class ClientManager implements Runnable {
                     break;
                 }*/
                 broadcastMessage(massageFromClient);
-            }
-            catch (IOException e){
+            } catch (IOException e) {
                 closeEverything(socket, bufferedReader, bufferedWriter);
                 break;
             }
         }
     }
 
-    private void broadcastMessage(String message){
-        for (ClientManager client: clients) {
-            try {
-                if (!client.name.equals(name)) {
-                    client.bufferedWriter.write(message);
-                    client.bufferedWriter.newLine();
-                    client.bufferedWriter.flush();
+    private void broadcastMessage(String message) {
+        if (privateMessage(message)) {
+            sendPrivateMessage(message);
+        } else {
+            for (String key : clients.keySet()) {
+                if (!key.equals(name)) {
+                    ClientManager clientManager = clients.get(key);
+                    sendMessage(clientManager,message);
                 }
             }
-            catch (IOException e){
-                closeEverything(socket, bufferedReader, bufferedWriter);
-            }
+        }
+    }
+
+    private void sendPrivateMessage(String message) {
+        String userName = message.split(" ")[1].substring(1);
+        ClientManager clientManager = clients.get(userName);
+        sendMessage(clientManager, message);
+    }
+
+    /**
+     * Метод определяет указан ли конкретный пользователь в сообщени при помощи "@"
+     * @param message сообщение
+     * @return true - если указан конкретный получатель, false - если сообщение для всех пользователей
+     */
+    private boolean privateMessage(String message) {
+        return message.split(" ")[1].startsWith("@");
+    }
+
+    /**
+     * Метод осуществляет отправку сообщения подключенному пользователю
+     * @param clientManager пользователь
+     * @param message сообщение
+     */
+    private void sendMessage(ClientManager clientManager, String message) {
+        try {
+            clientManager.bufferedWriter.write(message);
+            clientManager.bufferedWriter.newLine();
+            clientManager.bufferedWriter.flush();
+        } catch (IOException e) {
+            closeEverything(socket, bufferedReader, bufferedWriter);
         }
     }
 
@@ -88,8 +117,8 @@ public class ClientManager implements Runnable {
         }
     }
 
-    private void removeClient(){
-        clients.remove(this);
+    private void removeClient() {
+        clients.remove(name);
         System.out.println(name + " покинул чат.");
         broadcastMessage("Server: " + name + " покинул чат.");
     }
